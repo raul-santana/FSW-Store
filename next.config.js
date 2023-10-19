06726adb41ec352d95f74dcbbc2d77223819fbdf
Raul Santana <ti.raul77@gmail.com>
2023-10-18 23:24:28 -0300

    chore: add s3 domainto next config file

--- next.config.js
+++ next.config.js
@@ -1,19 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'lh3.googleusercontent.com',
-            port: '',
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-            port: '',
-          },
-        ],
-      },
+    // images: {
+    //     remotePatterns: [
+    //       {
+    //         protocol: 'https',
+    //         hostname: 'lh3.googleusercontent.com',
+    //         port: '',
+    //       },
+    //       {
+    //         protocol: 'https',
+    //         hostname: 'fsw-store.s3.sa-east-1.amazonaws.com',
+    //         port: '',
+    //       },
+    //       {
+    //         protocol: 'https',
+    //         hostname: 'images.unsplash.com',
+    //         port: '',
+    //       },
+    //     ],
+    //   },
+    images:{
+      domains: ["lh3.googleusercontent.com", "fsw-store.s3.sa-east-1.amazonaws.com"]
+    }
 }
 
 module.exports = nextConfig
