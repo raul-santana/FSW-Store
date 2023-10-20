b9f210e4c9714535b6701b48864ced59086e4712
Raul Santana <ti.raul77@gmail.com>
2023-10-20 08:14:49 -0300

    feat: ensure user goes to product details page on product click

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,6 +2,7 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
     product: ProductWithTotalPrice
@@ -9,54 +10,56 @@ interface ProductItemProps {
 
 const ProductItem = ({product}: ProductItemProps) => {
     return ( 
-        <div className="flex flex-col gap-4">
-            <div className="relative bg-accent rounded-lg h-[170px] w-full flex items-center justify-center">
-                <Image 
-                    src={product.imageUrls[0]}
-                    height={0}
-                    width={0}
-                    sizes="100vw"
-                    className="h-[90px] max-h-[70%] w-auto max-w-[80%]"
-                    style={{
-                        objectFit: "contain",
-                    }}
-                    alt={product.name}
-                />
+        <Link href={`/product/${product.slug}`}>
+            <div className="flex flex-col gap-4">
+                <div className="relative bg-accent rounded-lg h-[170px] w-full flex items-center justify-center">
+                    <Image 
+                        src={product.imageUrls[0]}
+                        height={0}
+                        width={0}
+                        sizes="100vw"
+                        className="h-[90px] max-h-[70%] w-auto max-w-[80%]"
+                        style={{
+                            objectFit: "contain",
+                        }}
+                        alt={product.name}
+                    />
 
-                {product.discountPercentage > 0 && (
-                    <Badge className="absolute left-3 top-3 px-2 py-[2px]">
-                        <ArrowDownIcon size={14} />
-                        {product.discountPercentage}%
-                    </Badge>
-                )}
-            </div>
-
-
-            <div>
-                <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
-                    {product.name}
-                </p>
-
-                <div className="flex items-end gap-2">
-                    {product.discountPercentage > 0 ? (
-                        <>
-                            <p className="font-semibold text-sm">
-                                R$ {product.totalPrice.toFixed(2)}
-                            </p>
-
-                            <p className="text-xs opacity-60 line-through">
-                                 R${Number(product.basePrice).toFixed(2)}
-                            </p>
-                        </>
-                    ) : (
-                        <p className="font-semibold text-sm">
-                        R${Number(product.basePrice).toFixed(2)}
-                        </p>
+                    {product.discountPercentage > 0 && (
+                        <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+                            <ArrowDownIcon size={14} />
+                            {product.discountPercentage}%
+                        </Badge>
                     )}
+                </div>
 
+
+                <div>
+                    <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+                        {product.name}
+                    </p>
+
+                    <div className="flex items-end gap-2">
+                        {product.discountPercentage > 0 ? (
+                            <>
+                                <p className="font-semibold text-sm">
+                                    R$ {product.totalPrice.toFixed(2)}
+                                </p>
+
+                                <p className="text-xs opacity-60 line-through">
+                                    R${Number(product.basePrice).toFixed(2)}
+                                </p>
+                            </>
+                        ) : (
+                            <p className="font-semibold text-sm">
+                            R${Number(product.basePrice).toFixed(2)}
+                            </p>
+                        )}
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
      );
 }
  
