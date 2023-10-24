470aaf16320dec39ed8893719e6f48d95ba1fb56
Raul Santana <ti.raul77@gmail.com>
2023-10-24 00:16:44 -0300

    feat: add stripe webhook

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,86 +7,90 @@ import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
-import { createChecktout } from "@/actions/checkout";
+import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
+  const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
-    const { products, subtotal, total, totalDiscount } = useContext(CartContext)
+  const handleFinishPurchaseClick = async () => {
+    const checkout = await createCheckout(products);
 
-    const handleFinishPurchaseClick = async () => {
-        const checkout = await createChecktout(products);
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
-        const stripe = await loadStripe(
-            process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
-        );
+    stripe?.redirectToCheckout({
+      sessionId: checkout.id,
+    });
+  };
 
-        stripe?.redirectToCheckout({
-            sessionId: checkout.id
-        });
-    }
+  return (
+    <div className="flex h-full flex-col gap-8">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShoppingCartIcon size={16} />
+        Carrinho
+      </Badge>
 
-    return ( 
-        <div className="h-full flex flex-col gap-8">
-            <Badge className="w-fit gap-1 uppercase text-base border-2 border-primary px-3 py-[0.375rem]" variant="outline">
-                <ShoppingCartIcon size={16} />
-                Carrinho
-            </Badge>
-            
-            
-            <div className="h-full max-h-full flex flex-col gap-5 overflow-hidden">
-                <ScrollArea className="h-full">
-                    <div className="h-full flex flex-col gap-8">
-                        {products.length > 0 ? (
-                            products.map((product) => (
-                                <CartItem key={product.id} product={computeProductTotalPrice(product as any) as any}/>
-                            ))
-                        )
-                        :
-                        (
-                            <p>Você ainda não tem nenhum produto no carrinho.</p>
-                        )}
-                    </div>
-                </ScrollArea>
-            </div>
+      {/* RENDERIZAR OS PRODUTOS */}
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-8">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
+              </p>
+            )}
+          </div>
+        </ScrollArea>
+      </div>
 
-            <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3">
+        <Separator />
 
-                <Separator />
-
-                <div className="flex items-center justify-between text-xs">
-                    <p>Subtotal</p>
-                    <p>R$ {subtotal.toFixed(2)}</p>
-                </div>
-
-                <Separator />
-
-                <div className="flex items-center justify-between text-xs">
-                    <p> Entrega </p>
-                    <p className="uppercase"> Grátis </p>
-                </div>
-
-                <Separator />
-
-                <div className="flex items-center justify-between text-xs">
-                    <p> Descontos </p>
-                    <p> - R$ {totalDiscount.toFixed(2)} </p>
-                </div>
-
-                <Separator />
-
-                <div className="flex items-center justify-between text-sm font-bold">
-                    <p> Total </p>
-                    <p> R$ {total.toFixed(2)} </p>
-                </div>
-
-                <Button className="mt-7 font-bold uppercase" onClick={handleFinishPurchaseClick}>
-                    Finalizar compra
-                </Button>
-
-            </div>
+        <div className="flex items-center justify-between text-xs">
+          <p>Subtotal</p>
+          <p>R$ {subtotal.toFixed(2)}</p>
         </div>
-     );
-}
- 
+
+        <Separator />
+
+        <div className="flex items-center justify-between text-xs">
+          <p>Entrega</p>
+          <p>GRÁTIS</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-between text-xs">
+          <p>Descontos</p>
+          <p>- R$ {totalDiscount.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-between text-sm font-bold">
+          <p>Total</p>
+          <p>R$ {total.toFixed(2)}</p>
+        </div>
+
+        <Button
+          className="mt-7 font-bold uppercase"
+          onClick={handleFinishPurchaseClick}
+        >
+          Finalizar compra
+        </Button>
+      </div>
+    </div>
+  );
+};
+
 export default Cart;
