3e0f557533fe74841cfd33eb4ae09a77a17169b7
Raul Santana <ti.raul77@gmail.com>
2023-10-24 20:36:46 -0300

    fix: ensure width product image is responsive

--- src/app/catalog/components/category-item.tsx
+++ src/app/catalog/components/category-item.tsx
@@ -12,17 +12,14 @@ const CategoryItem = ({category}: CategoryItemProps) => {
     return (
         <Link href={`category/${category.slug}`} target="_blank">
             <div className="flex flex-col">
-                <div className="w-full h-[150px] flex items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
+                <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
                     <Image 
                         src={category.imageUrl}
                         alt={category.name}
                         width={0}
                         height={0}
                         sizes="100vw"
-                        className="h-auto max-h-[70%] w-auto max-w-[80%]"
-                        style={{
-                            objectFit: "contain"
-                        }}
+                        className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
                     />
                 </div>
 
