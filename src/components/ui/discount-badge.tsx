bfb78daebfbf5305224e37ebef74a64025e862f5
Raul Santana <ti.raul77@gmail.com>
2023-10-26 16:13:59 -0300

    style: add text percent % to DiscountBadge component

--- src/components/ui/discount-badge.tsx
+++ src/components/ui/discount-badge.tsx
@@ -6,7 +6,7 @@ const DiscountBadge = ({children, className, ...props}: BadgeProps) => {
     return ( 
         <Badge className={twMerge("px-2 py-[2px]", className)} {...props}>
             <ArrowDownIcon size={14} />
-            {children}
+            {children}%
         </Badge>
      );
 }
